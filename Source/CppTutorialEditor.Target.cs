25f4ce22b95c0340675b825bfe2f1422d0600bd9
SimonLee <2335823994@qq.com>
2024-05-14 14:52:34 +0800

    Editor Module and Component Visualization

--- Source/CppTutorialEditor.Target.cs
+++ Source/CppTutorialEditor.Target.cs
@@ -5,11 +5,11 @@ using System.Collections.Generic;
 
 public class CppTutorialEditorTarget : TargetRules
 {
-	public CppTutorialEditorTarget( TargetInfo Target) : base(Target)
+	public CppTutorialEditorTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
 		DefaultBuildSettings = BuildSettingsVersion.V4;
 		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_3;
-		ExtraModuleNames.Add("CppTutorial");
+		ExtraModuleNames.AddRange(new string[] {"CppTutorial", "CppTutorialEditor"});
 	}
 }
